6a4645faec80d5c06cb9b39a7b53c3bd9cd5b2e3
杉浦義宏 <sugiura_yoshihiro@tigermonday.com>
2024-08-19 09:41:03 +0000

    参照元が重複するため、m_ga4_sessionに参照元が存在する場合はそれを採用。

--- includes/ga4/constants.js
+++ includes/ga4/constants.js
@@ -2,6 +2,12 @@ const GA4_DATABASE = 'molts-data-project';
 const GA4_DATASET = 'analytics_218861058';
 const GA4_TABLE = 'events_*';
 const GA4_INTRADAY_TABLE = 'events_intraday_*';
+// データセット
+const CLEANSE = 'df_molts_ga4_cleanse';
+const MART = 'df_molts_ga4_mart';
+const REPORT = 'df_molts_ga4_report';
+const SOURCE = 'df_molts_ga4_source';
+const STAGING = 'df_molts_ga4_staging';
 // 集計対象ホスト名 definitions/ga4/staging/s_ga4_events_exclude_internal.sqlxでホスト名の指定を行うが、ホスト名が複数必要な場合はここに追加し、s_ga4_events_exclude_internalにも追加すること
 const HOSTNAME1 = 'moltsinc.co.jp';
 const HOSTNAME2 = 'moltsinc.co.jp';  
@@ -11,4 +17,4 @@ const MP_EVENT2 = 'Measurement Protocol用イベント2';
 // report/r_ga4_conversion.sqlx 内のコンバージョンページURL
 const CV_PAGE_LOCATION = 'https://moltsinc.co.jp/%thanks%';
 
-module.exports = {GA4_DATABASE, GA4_DATASET, GA4_TABLE, GA4_INTRADAY_TABLE, HOSTNAME1, HOSTNAME2,  MP_EVENT1, MP_EVENT2, CV_PAGE_LOCATION}
+module.exports = {GA4_DATABASE, GA4_DATASET, GA4_TABLE, GA4_INTRADAY_TABLE, CLEANSE, MART, REPORT ,SOURCE, STAGING, HOSTNAME1, HOSTNAME2,  MP_EVENT1, MP_EVENT2, CV_PAGE_LOCATION}
