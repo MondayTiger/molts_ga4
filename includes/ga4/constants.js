0eda159f06d5c59901515f2ef72166e2ee08b627
杉浦義宏 <sugiura_yoshihiro@tigermonday.com>
2024-10-04 08:16:01 +0000

    constants.GA4_FIRST_DATEを追加

--- includes/ga4/constants.js
+++ includes/ga4/constants.js
@@ -21,6 +21,8 @@ const MP_EVENT2 = 'Measurement Protocol用イベント2';
 // report/r_ga4_conversion.sqlx 内のコンバージョンページURL
 const CV_PAGE_LOCATION = 'https://moltsinc.co.jp/%thanks%';
 // デフォルトチャネルグループのテーブル名（共通）　https://docs.google.com/spreadsheets/d/1gu6JfV0PD9QgfzPZT5EUCCvh1YHtENvpCENogSrxtd4/edit?gid=0#gid=0
-const CHANNEL_GROUP_TABLE = 'molts-data-project.general_master_us.ga4_channel_grouping_base'
+const CHANNEL_GROUP_TABLE = 'molts-data-project.general_master_us.ga4_channel_grouping_base';
+// GA4データの最も古い日
+const GA4_FIRST_DATE = '2020-1-1';
 
-module.exports = {GA4_DATABASE, GA4_DATASET, GA4_TABLE, GA4_INTRADAY_TABLE,PROJECT, CLEANSE, MART, REPORT ,SOURCE, STAGING, HOSTNAME1, HOSTNAME2,  MP_EVENT1, MP_EVENT2, CV_PAGE_LOCATION, CHANNEL_GROUP_TABLE}
+module.exports = {GA4_DATABASE, GA4_DATASET, GA4_TABLE, GA4_INTRADAY_TABLE,PROJECT, CLEANSE, MART, REPORT ,SOURCE, STAGING, HOSTNAME1, HOSTNAME2,  MP_EVENT1, MP_EVENT2, CV_PAGE_LOCATION, CHANNEL_GROUP_TABLE,GA4_FIRST_DATE}
